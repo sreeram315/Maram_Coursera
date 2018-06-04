07d32a260d5bfa4e0e07c1c74fd4b208f9402cf2
sreeram <sreerammaram2@gmail.com>
2018-06-04 23:42:23 +0530

    Adding code outline and documentation for C1M1 Assessment

--- stats.h
+++ stats.h
@@ -21,7 +21,86 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief Prints the minimum, maximum, mean and median of the array of elements
+ * 
+ *  This function takes an input of array and its size, finds the minimum, maximum, mean and median of 
+ *  the provided array of elements and prints them on the screen.
+ * 
+ *@param 1) Array whose statistics are to be printed, 2) size of the same array
+ *@return void
+ */
+void print_statistics(unsigned char *a,int n)
+
+/**
+ * @brief Prints the contents of an array
+ * 
+ *  This function takes an input of array and prints all the contents of the array in the same order. 
+ *  
+ * 
+ *@param 1) Array whose values to be printed, 2) size of the same array
+ *@return void
+ */
+
+void print_array(unsigned char *a, int n)
+
+/**
+ * @brief Returns the median of given array
+ * 
+ *  This function takes an input of array and its size, finds the median and returns the value. 
+ *  
+ * 
+ *@param 1) Array whose median to be calculated, 2) size of the same array
+ *@return median of given array
+ */
+
+int find_median(unsigned char *a,int n)
+
+/**
+ * @brief Returns the mean of given array
+ * 
+ *  This function takes an input of array and its size, finds the mean and returns the value. 
+ *  
+ * 
+ *@param 1) Array whose mean to be calculated, 2) size of the same array
+ *@return median of given array
+ */
+
+int find_mean(unsigned char *a,int n)
+
+/**
+ * @brief Returns the maximum number of given array
+ * 
+ *  This function takes an input of array and its size, finds the maximum and returns the value. 
+ *  
+ * 
+ *@param 1) Array whose maximum is to be calculated, 2) size of the same array
+ *@return maximum of given array
+ */
+
+int find_maximum(unsigned char *a, int n)
+
+/**
+ * @brief Returns the minimum of given array of elements
+ * 
+ *  This function takes an input of array and its size, finds the minimum and returns the value. 
+ *  
+ * 
+ *@param 1) Array whose minimum is to be calculated, 2) size of the same array
+ *@return minimum of the given array
+ */
+
+int find_minimum(unsigned char *a, int n)
+/**
+ * @brief Sorts the given array in descending order
+ * 
+ *  This function takes an input of array and its size, sorts it from largest to smallest in the  
+ *  address it already is(original array)
+ * 
+ *@param 1) Array whose values to be calculated, 2) size of the same array
+ *@return void
+ */
+void sort_array(unsigned char *a,int n)
 
 /**
  * @brief <Add Brief Description of Function Here>
